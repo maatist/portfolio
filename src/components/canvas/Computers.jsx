e0a20b3aef8c479935557957b6cb6676c7b43389
Matias Sepulveda <matias.seto@gmail.com>
2023-08-23 19:06:56 -0400

    motion.div para scroll a about - mediaQuery para el modelo

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,7 @@ import { OrbitControls, Preload, meshBounds, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({isMobile}) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
 
 
@@ -20,7 +20,7 @@ const Computers = () => {
           intensity={1}
         />
         <spotLight 
-          power={50000}
+          power={60000}
           position={[-20, 50, 10]}
           angle={0.12}
           penumbra={1}
@@ -30,8 +30,8 @@ const Computers = () => {
         />
         <primitive 
           object={computer.scene}
-          scale={0.65}
-          position={[0, -2.25, -1.5]}
+          scale={isMobile ? 0.60 : 0.70}
+          position={isMobile ? [0, -2, -1.5] : [0, -2.25, -1.5]}
           rotation={[-0.01, -0.2, -0.1]}
         />
 
@@ -40,6 +40,23 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false)
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 640px)')
+    setIsMobile(mediaQuery.matches)
+    
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+    mediaQuery.addEventListener('change',handleMediaQueryChange)
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+
+
   return (
     <Canvas
       frameloop="demand"
@@ -54,7 +71,7 @@ const ComputersCanvas = () => {
           minPolarAngle={Math.PI / 2}
         />
         <mesh>
-          <Computers />
+          <Computers isMobile={isMobile} />
         </mesh>
       </Suspense>
       <Preload all />
